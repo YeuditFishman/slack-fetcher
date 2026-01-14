a368ad2d8f36ec43d125d55068ef2a548a2ea099
y. fishman <yehuditfi@skyvar.co.il>
2026-01-14 15:15:22 +0200

    :white_check_mark:Fix CI

--- test/test_slack.py
+++ test/test_slack.py
@@ -4,20 +4,35 @@ from src.main import get_slack_users, save_users_to_json
 import unittest
 from unittest.mock import patch
 
+
 class TestSlackUsers(unittest.TestCase):
 
     @patch('src.main.requests.get')
+    @patch.dict(os.environ, {
+        "SLACK_TOKEN": "fake-token",
+        "SLACK_API_URL": "https://fake-url"
+        })
     def test_get_slack_users(self, mock_get):
         mock_get.return_value.status_code = 200
         mock_get.return_value.json.return_value = {
             "ok": True,
             "members": [
-                {"id": "U123", "name": "rikif", "real_name": "Riki F", "is_bot": False},
-                {"id": "U124", "name": "yehuditfi", "real_name": "Yehudit F", "is_bot": False}
+                {
+                    "id": "U123",
+                    "name": "rikif",
+                    "real_name": "Riki F",
+                    "is_bot": False
+                },
+                {
+                    "id": "U124",
+                    "name": "yehuditfi",
+                    "real_name": "Yehudit F",
+                    "is_bot": False
+                }
             ]
         }
 
-        users = get_slack_users("fake-token")
+        users = get_slack_users()
         self.assertIsInstance(users, list)
         self.assertGreater(len(users), 0)
         user = users[0]
@@ -26,17 +41,25 @@ class TestSlackUsers(unittest.TestCase):
         self.assertIn("full_name", user)
         self.assertIn("is_bot", user)
 
+    @patch.dict(os.environ, {
+        "SLACK_USERS_FILE": "test_users.json"
+        })
     def test_save_users_to_json(self):
         users = [
-            {"id": "U123", "username": "rikif", "full_name": "Riki F", "is_bot": False}
+            {
+                "id": "U123",
+                "username": "rikif",
+                "full_name": "Riki F",
+                "is_bot": False
+            }
         ]
-        filename = "test_users.json"
-        save_users_to_json(users, filename)
-        self.assertTrue(os.path.exists(filename))
-        with open(filename, "r", encoding="utf-8") as f:
+        save_users_to_json(users)
+        self.assertTrue(os.path.exists("test_users.json"))
+        with open("test_users.json", "r", encoding="utf-8") as f:
             data = json.load(f)
             self.assertEqual(data, users)
-        os.remove(filename)
+        os.remove("test_users.json")
+
 
 if __name__ == "__main__":
     unittest.main()
